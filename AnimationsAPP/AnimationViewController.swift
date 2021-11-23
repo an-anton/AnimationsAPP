7d1a722ae86c4458014b83a3cddce8605a555636
Ananev Anton <an-anton88@mail.ru>
2021-11-24 01:44:33 +0300

    создание интерфейса

---
+++ AnimationsAPP/AnimationViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AnimationViewController.swift
+//  AnimationsAPP
+//
+//  Created by Anton Anan'eV on 24.11.2021.
+//
+
+import UIKit
+
+class AnimationViewController: UIViewController {
+    
+     // MARK: - @IBOutlets
+    @IBOutlet var animationView: UIView!
+    @IBOutlet var animationLabel: UILabel!
+    @IBOutlet var curveLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+     // MARK: - Actions
+    @IBAction func buttonShowAnimatioin(_ sender: UIButton) {
+        
+    }
+    
+}
